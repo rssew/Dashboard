e965c308c20a80b4d6c9fae896d457d959ca497b
rssew <wingringo@gmail.com>
2025-12-16 05:01:44 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,11 @@
+import pandas as pd
 import streamlit as st
-from streamlit_option_menu import option_menu
 
-st.title('Hello, Students!')
-st.write('This is your Python Programming course.')
+data = {
+    'Product': ['A', 'B', 'C'],
+    'Sales': [1200, 850, 950],
+    'Customers': [300, 400, 350]
+}
+df = pd.DataFrame(data)
 
-with st.sidebar:
-    selected=option_menu(
-        menu_title = "Menu",
-        options = ["Home", "About", "Contact"],
-        icons = ["1-circle-fill",
-                 "2-circle-fill",
-                 "3-circle-fill"],
-        menu_icon= "emoji-smile-fill",
-        default_index=0,
-    )
-
-if selected == "Home":
-    st.title(f"Welcome to the {selected} page.")
-
-if selected == "About":
-    st.title(f"Welcome to the {selected} page.")
-
-if selected == "Contact":
-    st.title(f"Welcome to the {selected} page.")
+st.dataframe(df)
