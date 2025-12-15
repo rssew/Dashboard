c00649273a4649b950646bb0415e592fc890dfb2
rssew <wingringo@gmail.com>
2025-12-15 20:07:11 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,4 +8,4 @@ st.write('Please enter the monthly sales target and select the region.')
 
 
 
-target = st.number_input('Enter Monthly Sales Target (in USD)')
+target = st.number_input('Enter Monthly Sales Target (in USD)', value = 0)
