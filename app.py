f208903adfd116b956d68b724e5d413fbd51813f
rssew <wingringo@gmail.com>
2025-12-15 20:06:40 +0800

    Update app.py

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ st.title('Retail Business Dashboard')
 st.header('Manager Input Section')
 
 st.write('Please enter the monthly sales target and select the region.')
+
+
+
+target = st.number_input('Enter Monthly Sales Target (in USD)')
