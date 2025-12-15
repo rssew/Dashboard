1099bd8eab690c0837e0fc16ed9cb71f773c76c4
rssew <wingringo@gmail.com>
2025-12-15 20:08:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.header('Manager Input Section')
 
 st.write('Please enter the monthly sales target and select the region.')
 
-
-
 target = st.number_input('Enter Monthly Sales Target (in USD)', value = 0)
+
+region = st.selectbox('Select Region:', ['North', 'South', 'West', 'East'])
