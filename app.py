d90a599304f3b01e99504f4cca2051802464dacb
rssew <wingringo@gmail.com>
2025-12-15 20:10:57 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,3 +9,6 @@ st.write('Please enter the monthly sales target and select the region.')
 target = st.number_input('Enter Monthly Sales Target (in USD)', value = 0)
 
 region = st.selectbox('Select Region:', ['North', 'South', 'West', 'East'])
+
+if st.button('Submit'):
+  st.write(f'Monthly Target Sales: {target} \n Region: {region}')
